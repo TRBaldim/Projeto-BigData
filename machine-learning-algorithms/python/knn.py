39c2b6353fb75b031a618e59ded1b3ac643fc835
TRBaldim <thiagorbaldim@gmail.com>
2017-03-16 15:10:45 -0300

    Code for calculate the euclidian distance via Pure Python and all native Library and application to Iris dataset from UCI Machine Learning Datasets

--- machine-learning-algorithms/python/knn.py
+++ machine-learning-algorithms/python/knn.py
@@ -1,9 +1,18 @@
 from test_math import *
 
 # Dataset was loaded from http://archive.ics.uci.edu/ml/datasets/Iris
-#data_file = open('../datasets/iris.data')
+data_file = open('../datasets/iris.data')
 
-#for i in data_file:
-#    print(i)
+data_matrix = []
+for i in data_file:
+    data_list = [float(k) for k in i.split(',')[:3]]
+    data_matrix.append(data_list)
 
-print calc_euclidian_distance([1, 2], [3, 4])
+# Calculate and sort distance
+distance_array = []
+for i in data_matrix:
+    distance_array.append(calc_euclidian_distance(i, [3.5, 4.1, 5.6]))
+
+# sorted Function Uses Timsort for ordering the data
+# https://en.wikipedia.org/wiki/Timsort
+print sorted(distance_array)
--- machine-learning-algorithms/python/test_math/euclidian.py
+++ machine-learning-algorithms/python/test_math/euclidian.py
@@ -3,6 +3,12 @@ import itertools
 
 
 def calc_euclidian_distance(vec_a, vec_b):
+    '''
+    Calculate the Euclidian Distance of two arrays of the same size
+    :param vec_a: Array of Numbers
+    :param vec_b: Array of Number
+    :return: float value of Euclidian distance
+    '''
     # https://en.wikipedia.org/wiki/Euclidean_distance
     sum_data = 0
     for a, b in itertools.izip(vec_a, vec_b):
