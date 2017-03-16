36309801434a754e8aee6c94ece346145a0047e8
TRBaldim <thiagorbaldim@gmail.com>
2017-03-16 15:41:46 -0300

    First dummy implementation of KNN

--- machine-learning-algorithms/python/euclidian.py
+++ machine-learning-algorithms/python/euclidian.py
@@ -5,14 +5,13 @@ data_file = open('../datasets/iris.data')
 
 data_matrix = []
 for i in data_file:
-    split_row = i.replace('\n', '').split(',')
-    data_list = ([float(k) for k in split_row[:4]], split_row[4])
+    data_list = [float(k) for k in i.split(',')[:4]]
     data_matrix.append(data_list)
 
 # Calculate and sort distance
 distance_array = []
 for i in data_matrix:
-    distance_array.append(euclidian_knn([3.5, 4.1, 5.6, 1.4], i))
+    distance_array.append(calc_euclidian_distance([3.5, 4.1, 5.6], i))
 
 # sorted Function Uses Timsort for ordering the data
 # https://en.wikipedia.org/wiki/Timsort
--- machine-learning-algorithms/python/test_math/euclidian.py
+++ machine-learning-algorithms/python/test_math/euclidian.py
@@ -14,3 +14,13 @@ def calc_euclidian_distance(vec_a, vec_b):
     for a, b in itertools.izip(vec_a, vec_b):
         sum_data += (a - b) ** 2
     return math.sqrt(sum_data)
+
+def euclidian_knn(vec_a, tup_b):
+    '''
+    Calculate knn with euclidian Distance using KNN core algorithm
+    :param vec_a: Array of Numbers
+    :param tup_b: Tuple of Array of Numbers and the Label of the point
+    :return: A tuple of Value and Label
+    '''
+    vec_b, label = tup_b
+    return calc_euclidian_distance(vec_a, vec_b), label
