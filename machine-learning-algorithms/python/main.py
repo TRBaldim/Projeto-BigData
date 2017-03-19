4e170e497d72e5d3682645eaf7e3a591b19b6b99
thiago <thiagorbaldim@gmail.com>
2017-03-19 12:59:54 -0300

    Adding changes to the results at Process with Euclidian and Quadratic model

--- machine-learning-algorithms/python/main.py
+++ machine-learning-algorithms/python/main.py
@@ -1,10 +1,13 @@
 import random
 from algorithms import knn
 import matplotlib.pyplot as plt
+import matplotlib as mlt
 import time
 
+mlt.use('TkAgg')
+
 # Dataset was loaded from http://archive.ics.uci.edu/ml/datasets/Iris
-data_file = open('../datasets/fat_iris.data')
+data_file = open('../datasets/big_iris.data')
 
 data_matrix = []
 for i in data_file:
@@ -12,7 +15,7 @@ for i in data_file:
     data_list = ([float(k) for k in split_row[:4]], split_row[4])
     data_matrix.append(data_list)
 
-random.seed(42)
+random.seed(54654)
 random.shuffle(data_matrix)
 
 size = len(data_matrix)
@@ -20,13 +23,17 @@ size = len(data_matrix)
 data_test = data_matrix[int(size * 0.3):]
 data_matrix = data_matrix[:int(size * 0.7)]
 
-for _ in range(10):
-    result_data = []
-    start_time = time.clock()
-    for t in data_test:
-        res = (knn(t[0], data_matrix, 'quadratic'), t[1])
-        result_data.append(res)
-    print str(time.clock() - start_time) + ' Seconds'
+# Euclidian
+result_data = []
+c = 0
+start_time = time.clock()
+for t in data_test:
+    res = (knn(t[0], data_matrix, 'euclidian'), t[1])
+    result_data.append(res)
+    c += 1
+    if c % 100 == 0:
+        print c
+print str(time.clock() - start_time) + ' Seconds'
 
 count = 0.0
 right = 0.0
@@ -37,8 +44,31 @@ for i in result_data:
     graph_res.append(right / count)
 print right / count
 
+
+# Quadratic
+result_data = []
+start_time = time.clock()
+c = 0
+for t in data_test:
+    res = (knn(t[0], data_matrix, 'quadratic'), t[1])
+    result_data.append(res)
+    c += 1
+    if c % 100 == 0:
+        print c
+print str(time.clock() - start_time) + ' Seconds'
+
+count = 0.0
+right = 0.0
+graph_res_qudratic = []
+for i in result_data:
+    count += 1.0
+    right += 1.0 if i[0] == i[1] else 0.0
+    graph_res_qudratic.append(right / count)
+print right / count
+
 axes = plt.gca()
 axes.set_ylim([0.5, 1.0])
 plt.plot(graph_res)
+plt.plot(graph_res_qudratic)
 plt.ylabel('Precision')
 plt.show()
